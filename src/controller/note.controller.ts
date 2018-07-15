1d2b73803a85bfb3ff7379fd6c1ad73bf0961b49
m8524769 <m8524769@163.com>
2018-07-16 00:15:37 +0800

    New note component

---
+++ src/controller/note.controller.ts
@@ -0,0 +1,51 @@
+import * as Router from "koa-router";
+import { Context } from "koa";
+import { getManager, Repository } from "typeorm";
+import { Note } from "../entity/Note";
+import { GistService } from "../service/gist.service";
+
+export class NoteController {
+
+    router: Router;
+    private note: Repository<Note>;
+    private GistService: GistService
+
+    constructor() {
+
+        this.note = getManager().getRepository(Note);
+        this.GistService = new GistService();
+
+        this.router = new Router()
+
+            .param("id", (id: string, ctx: Context, next: Context): Context => {
+                ctx.id = id;
+                return next();
+            })
+
+            .post("/", async (ctx: Context) => {
+                const request = ctx.request.body;
+                ctx.body = await this.GistService.create(request.gist).then(response => {
+                    // console.log(response.gistId);
+                    return this.note.save(
+                        this.note.create({
+                            gistId: response.gistId,
+                            description: request.gist.description,
+                            subject: request.subject,
+                            author: request.author,
+                        })
+                    )
+                }).catch(error => error);
+            })
+
+            .get("/:id", async (ctx: Context) => {
+                ctx.body = await this.GistService.get(ctx.id)
+                    .catch(error => error);
+            })
+
+            // .put("/:id/star", async (ctx: Context) => {
+            //     ctx.body = await this.GistService.star(ctx.id);
+            // })
+
+    }
+
+}
