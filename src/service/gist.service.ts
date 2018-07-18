306f00caf4bb2349c22a9991ecdffa9a66ab6143
m8524769 <m8524769@163.com>
2018-07-19 00:29:32 +0800

    Update token

--- src/service/gist.service.ts
+++ src/service/gist.service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 axios.defaults.baseURL = "https://api.github.com/gists";
-axios.defaults.headers.common['Authorization'] = "token 61a473363cfcb5f520278417b8f9fc8cb1727e14";
+axios.defaults.headers.common['Authorization'] = "token 06a04a641972f90e51465909883d31691bbffdf6";
 
 export class GistService {
 
